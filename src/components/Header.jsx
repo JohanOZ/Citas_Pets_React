349eb2d2570761953a07161d43992730a6e83468
Johan Olea <johanolea@hotmail.com>
2022-07-13 00:47:26 -0500

    Segundo

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+
+
+    return (
+        <h1 className="text-2xl font-bold uppercase" >Desde header</h1>
+    )
+}
+
+export default Header;
---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function ListadoPacientes() {
+  return (
+    <div>
+        <h2>Listado Pacientes</h2>
+        <ul>
+            <li>
+                Johan Olea
+            </li>
+            <li>
+                Juan Olea
+            </li>
+            <li>
+                Jonathan Olea
+            </li>
+
+        </ul>
+    </div>
+  )
+}
+
+export default ListadoPacientes
